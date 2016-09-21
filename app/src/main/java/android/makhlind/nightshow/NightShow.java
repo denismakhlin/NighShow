79901a812ea4f8e2860455e7ef7371ff9e6ca9d9
Way too tall <denismakhlin@gmail.com>
2016-09-21 13:49:37 -0700

    5th commit

--- app/src/main/java/android/makhlind/nightshow/NightShow.java
+++ app/src/main/java/android/makhlind/nightshow/NightShow.java
@@ -2,12 +2,18 @@ package android.makhlind.nightshow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class NightShow extends AppCompatActivity {
+
+    private TextView answerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_night_show);
+
+        answerText = (TextView) findViewById(R.id.answerText);
+        answerText.setText("Reply is hazy, try again tomorrow");
     }
 }
---
+++ app/src/main/java/android/makhlind/nightshow/Predictions.java
@@ -0,0 +1,5 @@
+package android.makhlind.nightshow;
+
+
+public class Predictions {
+}
