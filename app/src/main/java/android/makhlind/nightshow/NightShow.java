e2ed341a8bfce82d9fe1573760ce25d4ee055013
Denis Makhlin <denismakhlin@gmail.com>
2016-10-07 13:37:11 -0700

    15

--- app/src/main/java/android/makhlind/nightshow/NightShow.java
+++ app/src/main/java/android/makhlind/nightshow/NightShow.java
@@ -1,5 +1,9 @@
 package android.makhlind.nightshow;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -7,6 +11,23 @@ import android.widget.TextView;
 public class NightShow extends AppCompatActivity {
 
     private TextView answerText;
+
+    private SensorManager sensorManager;
+    private float acceleration;
+    private float currentAcceleration;
+    private float previousAcceleration;
+
+    private final SensorEventListener sensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
