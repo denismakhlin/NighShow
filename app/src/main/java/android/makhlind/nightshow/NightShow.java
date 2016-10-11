c241ca9908cd088b27822d23b4bff1a96f025571
Denis Makhlin <denismakhlin@gmail.com>
2016-10-11 13:38:10 -0700

    20

--- app/src/main/java/android/makhlind/nightshow/NightShow.java
+++ app/src/main/java/android/makhlind/nightshow/NightShow.java
@@ -50,10 +50,12 @@ public class NightShow extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        sensorManager.registerListener(sensorListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        sensorManager.unregisterListener(sensorListener);
     }
 }
